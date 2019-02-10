65d239c0af9191563343b4cde9fdb1d50571af0c
treekey999 <treekey999@gmail.com>
2019-02-11 05:11:51 +0800

    implement confirmBox promise

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react'
 import './App.scss'
 
-import { ConfirmBox } from './promiseModal'
+import { confirmBox } from './promiseModal'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
-        <ConfirmBox />
-
         <button
           type='button'
           className='btn-example logout'
+          onClick={() => {
+            confirmBox({
+              color: 'primary',
+              title: 'Logout',
+              description: 'Are you sure to logout?',
+            }).then(() => {
+              console.log('%c Click Confirm!, prepare to logout ...', 'color: blue')
+            }).catch(() => {
+              console.log('%c Click Cancel!, nothing happened ...', 'color: blue')
+            }) 
+          }}
         >
           Logout
         </button>
@@ -20,6 +28,18 @@ class App extends Component {
         <button
           type='button'
           className='btn-example submit'
+          onClick={() => {
+            confirmBox({
+              color: 'info',
+              title: 'Submit Form',
+              description: 'Are you sure to submit form?',
+              confirmText: 'Submit',
+            }).then(() => {
+              console.log('%c Click Confirm!, prepare to fetch data to backend api ...', 'color: green')
+            }).catch(() => {
+              console.log('%c Click Cancel!, nothing happened ...', 'color: green')
+            }) 
+          }}
         >
           Submit
         </button>
@@ -27,6 +47,18 @@ class App extends Component {
         <button
           type='button'
           className='btn-example delete'
+          onClick={() => {
+            confirmBox({
+              color: 'danger',
+              title: 'Delete Product',
+              description: 'Are you sure to delete this product?',
+              confirmText: 'Delete',
+            }).then(() => {
+              console.log('%c Click Confirm!, prepare to fetch data to delete product ...', 'color: red')
+            }).catch(() => {
+              console.log('%c Click Cancel!, nothing happened ...', 'color: red')
+            }) 
+          }}
         >
           Destroy
         </button>
--- src/promiseModal/index.js
+++ src/promiseModal/index.js
@@ -1,7 +1,12 @@
 import './style.scss' // base modal overlay csss
 
+import promisableModal from './promisableModal'
+
 import ConfirmBox from './components/ConfirmBox'
 
+const confirmBox = props => promisableModal(ConfirmBox, props)
+
 export {
-  ConfirmBox,
+  promisableModal,
+  confirmBox,
 }
